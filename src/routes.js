07462c23fb6d2af86b8218c5dd441561d0323520
andrei <andreixhz123@gmail.com>
2020-08-31 13:11:56 -0300

    Imported Menu

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Services from './pages/Services';
+import Menu from './components/Menu/Index';
 
 function Routes() {
     return (
         <BrowserRouter>
+            <Menu/>
             <Route path="/" exact component={Home} />
             <Route path="/servicos" exact component={Services} />
         </BrowserRouter>
