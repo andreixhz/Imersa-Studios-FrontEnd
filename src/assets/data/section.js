ad65e1d42283bf5977dd549f130be9f898059835
andrei <andreixhz123@gmail.com>
2020-09-08 13:10:45 -0300

    section informations from services

---
+++ src/assets/data/section.js
@@ -0,0 +1,38 @@
+export default sections = {
+    marca:{
+        title:"Marca",
+        description:"Nossos design dedicados e qualificados, irão criar o melhor para você e o seu negocio.",
+        items:[
+            {
+                title:"Logotipos",
+                image: require("../../assets/images/services/im1.png")
+            },
+            {
+                title:"Manual de indentidade",
+                image: require("../../assets/images/services/im2.png")
+            },
+            {
+                title:"Redes Social",
+                image: require("../../assets/images/services/im2.png")
+            }
+        ]
+    },
+    sistemas:{
+        title:"Dev",
+        description:"Nossos programadores e desenvolvedores dedicados oara criar um programa de acordo com o seu planejamento, usando as melhores tecnologias do mercado",
+        items:[
+            {
+                title:"Web",
+                image: require("../../assets/images/services/im2.png")
+            },
+            {
+                title:"Sistemas",
+                image: require("../../assets/images/services/im2.png")
+            },
+            {
+                title:"Jogos",
+                image: require("../../assets/images/services/im2.png")
+            }
+        ]
+    },
+}
