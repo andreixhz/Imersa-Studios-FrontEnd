8fb01c075ad1f3c66e70317960ac1e4e4329566f
andrei <andreixhz123@gmail.com>
2020-10-28 08:42:58 -0300

    Section info

--- src/assets/data/section.js
+++ src/assets/data/section.js
@@ -1,38 +1,78 @@
-export default sections = {
+export default {
     marca:{
         title:"Marca",
         description:"Nossos design dedicados e qualificados, irão criar o melhor para você e o seu negocio.",
+        icon: require("../images/icons/Shield.svg"),
         items:[
             {
                 title:"Logotipos",
-                image: require("../../assets/images/services/im1.png")
+                image: require("../images/services/im1.png")
             },
             {
                 title:"Manual de indentidade",
-                image: require("../../assets/images/services/im2.png")
+                image: require("../images/services/im23.png")
             },
             {
                 title:"Redes Social",
-                image: require("../../assets/images/services/im2.png")
+                image: require("../images/services/im1.png")
             }
         ]
     },
     sistemas:{
         title:"Dev",
-        description:"Nossos programadores e desenvolvedores dedicados oara criar um programa de acordo com o seu planejamento, usando as melhores tecnologias do mercado",
+        description:"Nossos programadores e desenvolvedores dedicados para criar um programa ou aplicação de acordo com o seu planejamento, usando as melhores tecnologias do mercado",
+        icon: require("../images/icons/Www.svg"),
         items:[
             {
                 title:"Web",
-                image: require("../../assets/images/services/im2.png")
+                image: require("../images/services/im1.png")
             },
             {
                 title:"Sistemas",
-                image: require("../../assets/images/services/im2.png")
+                image: require("../images/services/im1.png")
             },
             {
                 title:"Jogos",
-                image: require("../../assets/images/services/im2.png")
+                image: require("../images/services/im1.png")
             }
         ]
     },
+    ilustrations:{
+        title:"Ilustrações",
+        description:"Usando os melhores conceitos de pintura e vetorização para deixar seu trabalho mais profissional",
+        icon: require("../images/icons/Pen.svg"),
+        items:[          
+            {
+                title:"Vetorial",
+                image: require("../images/services/im1.png")
+            },        
+            {
+                title:"Pintura Digital",
+                image: require("../images/services/im1.png")
+            },
+            {
+                title:"Conceitos",
+                image: require("../images/services/im1.png")
+            },
+        ]
+    },
+    model:{
+        title:"3D",
+        description:"Criamos modelos 3D tanto para jogos quanto para serviços de visualização 3D",
+        icon: require("../images/icons/3D.svg"),
+        items:[          
+            {
+                title:"Game Assets",
+                image: require("../images/services/im1.png")
+            },        
+            {
+                title:"Visualizaçãoes 3D",
+                image: require("../images/services/im1.png")
+            },
+            {
+                title:"Maquete eletrônica",
+                image: require("../images/services/im1.png")
+            },
+        ]
+    }
 }
